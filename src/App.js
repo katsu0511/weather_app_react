b6089a660eecacb898faeaf4bbe7b542b4c270d9
Katsuya Harada <haradakatsuya190511@gmail.com>
2025-01-19 17:44:24 -0800

    Revert "add Weather.js"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import Weather from './Weather';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Weather />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
