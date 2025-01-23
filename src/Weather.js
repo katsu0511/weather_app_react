7588e6c590852f0299070d3b30cdc0e954fac45a
Katsuya <haradakatsuya190511@gmail.com>
2025-01-23 11:03:36 -0800

    enable to select cities

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,12 @@
+import { useState } from "react";
 import { useQuery } from 'react-query';
 import text from './appid.txt';
 import cities from './city.json';
 
-const fetchWeather = async () => {
-  const appid = await getAppid();
-  const latitude = '35.6895';
-  const longitude = '139.6917';
-  const language = 'en';
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${language}&appid=${appid}`);
-  if (res.ok){ return res.json(); }
-  throw new Error(res.statusText);
-};
+const cityArray = [];
+for (let i = 0; i < cities.length; i++) {
+  cityArray.push(<option>{cities[i].name}</option>);
+}
 
 const getAppid = async () => {
   const data = await fetch(text);
@@ -18,6 +14,34 @@ const getAppid = async () => {
 };
 
 export default function Weather() {
+  const [city, setCity] = useState("Tokyo");
+
+  const fetchWeather = async () => {
+    const appid = await getAppid();
+    const longitude = getCoordinate().lon;
+    const latitude = getCoordinate().lat;
+    const language = 'en';
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${language}&appid=${appid}`);
+    if (res.ok){ return res.json(); }
+    throw new Error(res.statusText);
+  };
+
+  const getCoordinate = () => {
+    for (let i = 0; i < cities.length; i++) {
+      if (cities[i].name === city) {
+        return cities[i].coord;
+      }
+    }
+  };
+
+  const cityChange = () => {
+    const citySelect = document.getElementById('city_select');
+    if (citySelect.value !== '-') {
+      setCity(citySelect.value);
+      alert('changed');
+    }
+  }
+
   const { data, isLoading, isError, error } = useQuery('weather', fetchWeather);
 
   if (isLoading) {
@@ -28,15 +52,9 @@ export default function Weather() {
     return <p>Error: {error.message}</p>;
   }
 
-  const cityArray = [];
-  for (let i = 0; i < cities.length; i++) {
-    cityArray.push(<option>{cities[i].name}</option>);
-  }
-
   return (
     <div>
-      <select>
-        <option>-</option>
+      <select id="city_select" onChange={cityChange}>
         {cityArray}
       </select>
       <figure>
